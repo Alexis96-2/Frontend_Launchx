8dadb1408f5fec217f14ba383df8a8bdb9476aac
Alexis96-2 <rexwolf96@gmail.com>
2022-03-20 19:46:13 -0500

    elimine mucho css por no salir bien

--- pokedex - css_javascript_api/javascript/funciones.js
+++ pokedex - css_javascript_api/javascript/funciones.js
@@ -1,1 +1,25 @@
-console.log("Hola pokemaniacos")
+
+const imprimir = () => {
+    const pokeName = document.getElementById("pokeName");
+    let pokeInput = pokeName.value;
+    console.log("¡Vamos! " + pokeInput);
+    let pokemonBuscado = 'https://pokeapi.co/api/v2/pokemon/' + pokeInput;
+    fetch(pokemonBuscado).then((res) => {
+        //console.log(res);
+        return res.json();
+    }).then((data) => {
+        console.log(data);
+        //Almacenaremos los datos necesarios para mostrar
+
+        let nombrePoke = data.name; // nombre
+        let idPoke = data.id; // id del pokemon
+        let pokeBasicData = nombrePoke + " / #" + idPoke;// Almacenamos nombre e id
+        const tituloPoke = document.getElementById("Nombrepoke");
+        tituloPoke.textContent = pokeBasicData;
+
+        let pokeImg = data.sprites.front_default;// Buscamos el sprite del pokemon
+        console.log(pokeImg);
+        const pokeImage = document.getElementById("pokeImg");//Buscamos donde insertarlo
+        pokeImage.src = pokeImg;//Realizamos el cambio de imagen
+    })
+}
